287609665594418ed6558535d1303c177c065edb
grek-andrian <gretsko.andrian@gmail.com>
2016-06-13 23:55:48 +0300

    Add users

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
 before_action :find_post, only: [:show, :edit, :update, :destroy]
+before_action :authenticate_user!, expext: [:index, :show]
+
   def index
     @posts=Post.all.order("created_at DESC")
   end
@@ -9,11 +11,11 @@ before_action :find_post, only: [:show, :edit, :update, :destroy]
   end
 
   def new
-    @post=Post.new
+    @post=current_user.posts.build
   end
 
   def create
-    @post=Post.new(post_params)
+    @post=current_user.posts.build(post_params)
     if @post.save
       redirect_to @post
     else
